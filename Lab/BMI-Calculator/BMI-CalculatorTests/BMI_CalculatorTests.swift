7326fcef356e506f356aaff11acd590f3e889fae
David Aghassi <david.aghassi@gmail.com>
2016-01-15 09:44:57 -0500

    Contains the first lab and first class
    -  Contains the intro to swift workbook we did
    -  BMI Calculator app we did in our first lab

---
+++ Lab/BMI-Calculator/BMI-CalculatorTests/BMI_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  BMI_CalculatorTests.swift
+//  BMI-CalculatorTests
+//
+//  Created by David Aghassi on 1/15/16.
+//  Copyright © 2016 David Aghassi. All rights reserved.
+//
+
+import XCTest
+@testable import BMI_Calculator
+
+class BMI_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
