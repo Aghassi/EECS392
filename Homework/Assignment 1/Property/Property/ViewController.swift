6374f8482856c9fb95d20fc81914649b27c99462
David Aghassi <david.aghassi@gmail.com>
2016-01-20 13:56:00 -0500

    Added functionality for editing fields
    -  Fields will set next field so whole form is
    filled out
    -  Only print the whole form when every field is filled

--- Homework/Assignment 1/Property/Property/ViewController.swift
+++ Homework/Assignment 1/Property/Property/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Outlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var purchaseDataTextField: UITextField!
+    @IBOutlet weak var purchasePriceTextField: UITextField!
+    @IBOutlet weak var ownersNameTextField: UITextField!
+    
+    var fields: [UITextField] = []
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fields = [nameTextField, addressTextField, purchaseDataTextField, purchasePriceTextField, ownersNameTextField]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITextFieldDelegates
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Depending on the text field, we set it to the next textfield
+        switch textField {
+        case let x where x == nameTextField:
+            addressTextField.becomeFirstResponder()
+        case let x where x == addressTextField:
+            purchaseDataTextField.becomeFirstResponder()
+        case let x where x == purchaseDataTextField:
+            purchasePriceTextField.becomeFirstResponder()
+        case let x where x == purchasePriceTextField:
+            ownersNameTextField.becomeFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        return true
     }
-
-
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if checkAllFieldsFilled() {
+            let property = Property()
+            property.name = nameTextField.text!
+            property.address = addressTextField.text!
+            property.propertyData = purchaseDataTextField.text!
+            property.price = Double(purchasePriceTextField.text!)!
+            property.owner = ownersNameTextField.text!
+        }
+        else {
+            print("Please fill in all fields")
+        }
+    }
+    
+    /**
+     Checks all the text fields are filled
+    */
+    func checkAllFieldsFilled() -> Bool {
+        for field in fields {
+            if field.text!.isEmpty {
+                return false
+            }
+        }
+        
+        return true
+    }
 }
 
