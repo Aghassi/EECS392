8d0d3327be11cffaff251934bebe519441529b70
David Aghassi <david.aghassi@gmail.com>
2016-01-19 23:39:02 -0500

    Added UITextFieldDelegate and methods to view

--- Homework/Assignment 1/FoodTracker/FoodTracker/ViewController.swift
+++ Homework/Assignment 1/FoodTracker/FoodTracker/ViewController.swift
@@ -8,21 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    // Mark: Properties
+class ViewController: UIViewController, UITextFieldDelegate {
+    // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        <#code#>
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        <#code#>
+    }
+    
+    // MARK: Action
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handles the text field user input through delegate callbacks
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
